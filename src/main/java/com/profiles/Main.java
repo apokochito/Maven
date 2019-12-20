484f8e6c7d0c6a33e9e03ccaa495e5846a7f8147
apokochito <dpinto1@ucol.mx>
2019-12-20 13:46:58 -0600

    Finishing profiles

--- src/main/java/com/profiles/Main.java
+++ src/main/java/com/profiles/Main.java
@@ -1,4 +1,23 @@
 package com.profiles;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Main {
+    public static void main(String args[]){
+
+        Main app = new Main();
+        Properties properties = app.loadPropertiesFile("db.properties");
+    }
+
+    public Properties loadPropertiesFile(String filePath){
+        Properties properties = new Properties();
+        try(InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)){
+
+        }catch (IOException e){
+            System.out.println("Unable to load properties file: "+filePath);
+        }
+        return properties;
+    }
 }
